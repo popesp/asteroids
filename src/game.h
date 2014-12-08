5c17f20bdd335362ea43b1b1558981da9e5f0b83
Shawn <shawncsutherland@gmail.com>
2014-12-08 13:42:41 -0700

    Added general game header

---
+++ src/game.h
@@ -0,0 +1,37 @@
+#ifndef	GAME
+#define	GAME
+
+
+#include	"render/window.h"
+#include	"render/render.h"
+
+
+#define	GAME_DEFAULT_WIDTH		600
+#define	GAME_DEFAULT_HEIGHT		400
+#define	GAME_DEFAULT_X			100
+#define	GAME_DEFAULT_Y			100
+
+#define	GAME_CLEARCOLOR			0.f, 0.1f, 0.1f
+
+#define	GAME_TITLE				"Asteroids"
+
+#define	GAME_KEY_ESC			27
+
+#define	GAME_FLAG_NONE			0x00
+#define	GAME_FLAG_TERMINATED	0x01
+
+
+struct game
+{
+	struct window window;
+	struct renderer renderer;
+	
+	unsigned char flags;
+};
+
+
+int game_startup(int argc, char** argv);
+void game_mainloop(void);
+
+
+#endif
